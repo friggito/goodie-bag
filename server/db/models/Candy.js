a794bd627da8e824b369829ec840744e208479c7
Fullstack <fullstack@Fullstacks-MacBook-Air.local>
2019-02-08 15:25:30 -0500

    defined Candy model

--- server/db/models/Candy.js
+++ server/db/models/Candy.js
@@ -1,6 +1,25 @@
 const Sequelize = require('sequelize');
 const db = require('../database');
 
+
 module.exports = db.define('candy', {
   // define your model here!
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  },
+  quantity: {
+    type: Sequelize.INTEGER,
+    validate: {
+      max: 10
+    }
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    defaultValue: 'public/bg.jpg'
+  }
 });
